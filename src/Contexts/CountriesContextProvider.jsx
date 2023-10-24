e473f6cbaa998e68127d9874655641691f59fa6d
TheFabi8A <cafaruoc2000@hotmail.com>
2023-10-24 06:35:56 -0500

    clear logs

--- src/Contexts/CountriesContextProvider.jsx
+++ src/Contexts/CountriesContextProvider.jsx
@@ -11,8 +11,6 @@ CountriesContextProvider.propTypes = {
 export default function CountriesContextProvider({ children }) {
   const { dataCountries, isFetching } = useFetch();
 
-  console.log(dataCountries);
-
   const [searchQuery, setSearchQuery] = useState("");
 
   const [currentPage, setCurrentPage] = useState(1);
